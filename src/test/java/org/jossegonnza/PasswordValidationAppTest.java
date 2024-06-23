56c5786d9350ce70c6e2b79a085f9a263d737e0a
JosseGonnza <accion.quevedo@hotmail.com>
2024-06-23 20:04:21 +0100

    🔴 Password should be correct // Try again

--- src/test/java/org/jossegonnza/PasswordValidationAppTest.java
+++ src/test/java/org/jossegonnza/PasswordValidationAppTest.java
@@ -6,19 +6,10 @@ import org.junit.jupiter.api.Test;
 public class PasswordValidationAppTest {
 
     @Test
-    void password_should_be_at_least_8_characters_long_return_true() {
+    void password_should_be_correct() {
         PasswordValidation passwordValidation = new PasswordValidation();
 
-        Assertions.assertThat(passwordValidation.validation("12345678")).isEqualTo(true);
-        Assertions.assertThat(passwordValidation.validation("1234567890123")).isEqualTo(true);
-    }
-
-    @Test
-    void password_should_be_at_least_8_characters_long_return_false() {
-        PasswordValidation passwordValidation = new PasswordValidation();
-
-        Assertions.assertThat(passwordValidation.validation("1234567")).isEqualTo(false);
-        Assertions.assertThat(passwordValidation.validation("123")).isEqualTo(false);
+        Assertions.assertThat(passwordValidation.validation("Isgood_8")).isEqualTo(true);
     }
 
 }
