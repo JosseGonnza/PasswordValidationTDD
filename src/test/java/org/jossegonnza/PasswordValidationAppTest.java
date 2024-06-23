305df4f93a8e3477db8e0b84579fe32cc07314a8
JosseGonnza <accion.quevedo@hotmail.com>
2024-06-23 19:28:46 +0100

    Password should be at least 8 characters long

--- src/test/java/org/jossegonnza/PasswordValidationAppTest.java
+++ src/test/java/org/jossegonnza/PasswordValidationAppTest.java
@@ -1,38 +1,24 @@
 package org.jossegonnza;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class PasswordValidationAppTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public PasswordValidationAppTest(String testName )
-    {
-        super( testName );
+public class PasswordValidationAppTest {
+
+    @Test
+    void password_should_be_at_least_8_characters_long_return_true() {
+        PasswordValidation passwordValidation = new PasswordValidation();
+
+        Assertions.assertThat(passwordValidation.validation("12345678")).isEqualTo(true);
+        Assertions.assertThat(passwordValidation.validation("1234567890123")).isEqualTo(true);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( PasswordValidationAppTest.class );
+    @Test
+    void password_should_be_at_least_8_characters_long_return_false() {
+        PasswordValidation passwordValidation = new PasswordValidation();
+
+        Assertions.assertThat(passwordValidation.validation("1234567")).isEqualTo(false);
+        Assertions.assertThat(passwordValidation.validation("123")).isEqualTo(false);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
